40a69ddf994045dc8682ea62aa6cc6f1ccf6fc6d
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-02-29 20:26:10 -0300

    Corrigindo e adicionando header no projeto para que volte a funcionar

--- js/api-get.js
+++ js/api-get.js
@@ -1,78 +1,95 @@
 var api_key = "?api_key=f93d84a232e65befaa774a77d5f12ba9";
 var api_url_base = "https://api.themoviedb.org/3/";
 var api_url_base_image = "https://image.tmdb.org/t/p/";
-var api_language = "";
+var api_language = "en-US";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTNkODRhMjMyZTY1YmVmYWE3NzRhNzdkNWYxMmJhOSIsInN1YiI6IjYyZTEyODliNWNhNzA0MjYxYWVmZGQxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AfQeYfMCf2wYdaqE7fGmOQKkyGwCvzcvuMDIUitvAc0",
+  },
+};
 
 $(document).ready(function () {
   var api_main_slick = getInfo("movie", "now_playing", null, true);
-  $.getJSON(api_main_slick, function (data) {
-    $.each(data.results, function (i, item) {
-      var caminho_imagem = "";
-      $(
-        "<div class='lancamento' style='background-image: url(" +
-          getImage(780, item.backdrop_path) +
-          ")'>" +
-          /*+ "<img src='https://image.tmdb.org/t/p/original/" + caminho_imagem + "'></img>"*/ item.title +
-          "</div>"
-      ).appendTo(".slick-main-lancamentos");
-      return i < 5;
-    });
-    $(".slick-main-lancamentos").slick({
-      centerMode: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      arrows: false,
-      dots: true,
-      dotsClass: "paginacao",
-      appendDots: ".lancamento",
-    });
-  });
+  console.log(api_main_slick);
+  fetch(api_main_slick, options)
+    .then((data) => data.json())
+    .then((data) => {
+      $.each(data.results, function (i, item) {
+        var caminho_imagem = "";
+        $(
+          "<div class='lancamento' style='background-image: url(" +
+            getImage(780, item.backdrop_path) +
+            ")'>" +
+            /*+ "<img src='https://image.tmdb.org/t/p/original/" + caminho_imagem + "'></img>"*/ item.title +
+            "</div>"
+        ).appendTo(".slick-main-lancamentos");
+        return i < 5;
+      });
+      $(".slick-main-lancamentos").slick({
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        dots: true,
+        dotsClass: "paginacao",
+        appendDots: ".lancamento",
+      });
+    })
+    .catch((err) => console.error(err));
+
   var api_slick_popular = getInfo("movie", "popular", null, true);
-  $.getJSON(api_slick_popular, function (data) {
-    $.each(data.results, function (i, item) {
-      var caminho_imagem = "";
-      $(
-        "<div class='lancamento' style='background-image: url(" +
-          getImage(780, item.backdrop_path) +
-          ")'>" +
-          /*+ "<img src='https://image.tmdb.org/t/p/original/" + caminho_imagem + "'></img>"*/ item.title +
-          "</div>"
-      ).appendTo(".slick-popular");
-      return i < 20;
-    });
-    $(".slick-popular").slick({
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      arrows: true,
-      prevArrow:
-        '<button type="button" class="slick-arrow slick-prev"><i class="fa-solid fa-arrow-left i"></i></button>',
-      nextArrow:
-        '<button type="button" class="slick-arrow slick-next"><i class="fa-solid fa-arrow-right i"></i></button>',
-      responsive: [
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 3,
+  fetch(api_slick_popular, options)
+    .then((data) => data.json())
+    .then((data) => {
+      $.each(data.results, function (i, item) {
+        var caminho_imagem = "";
+        $(
+          "<div class='lancamento' style='background-image: url(" +
+            getImage(780, item.backdrop_path) +
+            ")'>" +
+            /*+ "<img src='https://image.tmdb.org/t/p/original/" + caminho_imagem + "'></img>"*/ item.title +
+            "</div>"
+        ).appendTo(".slick-popular");
+        return i < 20;
+      });
+      $(".slick-popular").slick({
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow:
+          '<button type="button" class="slick-arrow slick-prev"><i class="fa-solid fa-arrow-left i"></i></button>',
+        nextArrow:
+          '<button type="button" class="slick-arrow slick-next"><i class="fa-solid fa-arrow-right i"></i></button>',
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+            },
           },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+            },
           },
-        },
-      ],
-    });
-  });
+        ],
+      });
+    })
+    .catch((err) => console.error(err));
 });
 
 function getInfo(type, subcategory, item_id, language) {
-  return `${api_url_base}${type}/${subcategory}/${
-    item_id ? item_id + "/" : ""
-  }${api_key}${language ? api_language : ""}`;
+  return `${api_url_base}${type}/${subcategory}${
+    item_id ? `/${item_id}` + "/" : ""
+  }${language ? `?language=${api_language}` : ""}&page=1`;
 }
 
 function getImage(width_size, img_path) {
