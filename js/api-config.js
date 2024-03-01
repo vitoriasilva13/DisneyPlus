4077d3fc9c4a3a83b6b431a32131ec3e06c9996c
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-03-01 19:23:39 -0300

    Ajustes

--- js/api-config.js
+++
@@ -1,9 +0,0 @@
-var api_key = ""; //key da api da sua aplicação no TMDB
-
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${api_key}`,
-  },
-};
