0df7f5c5988981b2f60a5fc2966a4e468a988fb5
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-03-02 20:35:47 -0300

    Adicionando informações de cast e crew nos detalhes do filme

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
-var api_key = ""; //key da api da sua aplicação no TMDB
+var api_key =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTNkODRhMjMyZTY1YmVmYWE3NzRhNzdkNWYxMmJhOSIsInN1YiI6IjYyZTEyODliNWNhNzA0MjYxYWVmZGQxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AfQeYfMCf2wYdaqE7fGmOQKkyGwCvzcvuMDIUitvAc0"; //key da api da sua aplicação no TMDB
 var api_url_base = "https://api.themoviedb.org/3/";
 var api_url_base_image = "https://image.tmdb.org/t/p/";
 var api_language = "en-US";
@@ -21,4 +22,8 @@ function getImage(width_size, img_path) {
   return `${api_url_base_image}/${
     width_size ? "w" + width_size : "original"
   }/${img_path}`;
+}
+
+function getCrew(item_id) {
+  return `${api_url_base}/movie/${item_id}/credits`;
 }
--- js/pages.js
+++ js/pages.js
@@ -16,4 +16,22 @@ $(document).ready(function () {
       document.querySelectorAll(".nome")[0].innerHTML = data.original_title;
       document.querySelectorAll(".detalhes")[0].innerHTML = data.overview;
     });
+
+  fetch(getCrew(searchParams.get("id")), options)
+    .then((data) => data.json())
+    .then((data) => {
+      document.querySelectorAll(".diretor")[0].innerHTML = data.crew.filter(
+        ({ job }) => job === "Director"
+      )[0].name;
+    });
+
+  fetch(getCrew(searchParams.get("id")), options)
+    .then((data) => data.json())
+    .then((data) => {
+      let atoresHTML = "";
+      data.cast.slice(0, 10).forEach((ator) => {
+        atoresHTML += `<span class='tag actor'>${ator.name} como ${ator.character}</span>`;
+      });
+      document.querySelectorAll(".cast")[0].innerHTML = atoresHTML;
+    });
 });
