50290cffb33a03f8a6ef7d7cf8e4b943744072c0
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-03-02 20:55:12 -0300

    gitignore

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
-var api_key =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTNkODRhMjMyZTY1YmVmYWE3NzRhNzdkNWYxMmJhOSIsInN1YiI6IjYyZTEyODliNWNhNzA0MjYxYWVmZGQxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AfQeYfMCf2wYdaqE7fGmOQKkyGwCvzcvuMDIUitvAc0"; //key da api da sua aplicação no TMDB
+var api_key = ""; //key da api da sua aplicação no TMDB
 var api_url_base = "https://api.themoviedb.org/3/";
 var api_url_base_image = "https://image.tmdb.org/t/p/";
 var api_language = "en-US";
