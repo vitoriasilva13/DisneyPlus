e492cc8e331433e149ac8ee0c041c749352227ed
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-03-04 15:09:59 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,10 @@ var api_url_base = "https://api.themoviedb.org/3/";
 var api_url_base_image = "https://image.tmdb.org/t/p/";
 var api_language = "en-US";
 
+if (api_key == "") {
+  api_key = prompt("Enter bearer:");
+}
+
 const options = {
   method: "GET",
   headers: {
