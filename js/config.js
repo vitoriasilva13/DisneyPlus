c9c21b728219892b37137b0f2340c3662ff38cd9
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-03-06 20:07:44 -0300

    Corrigindo linguagem e tamanho de pôster

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var api_key = getCookie("bearer") != "" ? getCookie("bearer") : ""; //key da api da sua aplicação no TMDB
 var api_url_base = "https://api.themoviedb.org/3/";
 var api_url_base_image = "https://image.tmdb.org/t/p/";
-var api_language = "en-US";
+var api_language = "pt-BR";
 
 if (api_key == "") {
   api_key = prompt("Enter bearer:");
